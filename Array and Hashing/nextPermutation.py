e2750dbb12a1e9a972ae150bc8433a6a7138a373
AISHWARYA <50246502+aishwarya-modi@users.noreply.github.com>
2024-04-01 21:10:39 +0530

    Update nextPermutation.py

--- Array and Hashing/nextPermutation.py
+++ Array and Hashing/nextPermutation.py
@@ -14,10 +14,14 @@ The replacement must be in place and use only constant extra memory.
 
 Input: nums = [1,2,3]
 Output: [1,3,2]
+
+
 Example 2:
 
 Input: nums = [3,2,1]
 Output: [1,2,3]
+
+
 Example 3:
 
 Input: nums = [1,1,5]
